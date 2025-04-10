3ab83d7c04e4bfbd4a6f151d05322b1afd18eea7
NathanSaxe <59581764+NathanSaxe@users.noreply.github.com>
2025-04-10 14:53:37 -0700

    commit

--- Homework4/mymalloc.c
+++ Homework4/mymalloc.c
@@ -3,6 +3,33 @@
 
 mlist_t mlist;
 
+int main(int argc, char* arv[]){
+  void * p1 = mymalloc(10);
+  printMemList(mlist.head);
+  void * p2 = mymalloc(100);
+  printMemList(mlist.head);
+  void * p3 = mymalloc(200);
+  printMemList(mlist.head);
+  void * p4 = mymalloc(500);
+  printMemList(mlist.head);
+  myfree(p3); p3 = NULL;
+  printMemList(mlist.head);
+  myfree(p2); p2 = NULL;
+  printMemList(mlist.head);
+  void * p5 = mymalloc(150);
+  printMemList(mlist.head);
+  void * p6 = mymalloc(500);
+  printMemList(mlist.head);
+  myfree(p4); p4 = NULL;
+  printMemList(mlist.head);
+  myfree(p5); p5 = NULL;
+  printMemList(mlist.head);
+  myfree(p6); p6 = NULL;
+  printMemList(mlist.head);
+  myfree(p1); p1 = NULL;
+  printMemList(mlist.head);
+}
+
 void* mymalloc(size_t size){
   mblock_t* freeBlock = findFreeBlockOfSize(size);
   splitBlockAtSize(freeBlock, size);
