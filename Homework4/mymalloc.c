83c960e88e93ebdcec70b16be910a55413c66bba
Nathan <nathanmsaxe@gmail.com>
2025-04-11 03:30:19 +0000

    commit

--- Homework4/mymalloc.c
+++ Homework4/mymalloc.c
@@ -31,22 +31,30 @@ int main(int argc, char* arv[]){
 }
 
 void* mymalloc(size_t size){
-  void* p = sbrk(size);
-  if(p == (void*) -1){
-    strerror(errno);
+  mblock_t* freeBlock = findFreeBlockOfSize(size);
+  if(mlist.head == NULL){
+    mlist.head = growHeapBySize(size);
+    freeBlock = mlist.head;
+    freeBlock->size = size;
+    return &freeBlock->payload;
   }
-  return p;
+  if(freeBlock == NULL){
+    freeBlock = growHeapBySize(size);
+  }
+  splitBlockAtSize(freeBlock, size);
+  return &freeBlock->payload;
 }
 
 void myfree(void* ptr){
-  mblock_t* temp = mlist.head;
-	//ptr is, theoretically, located somewhere within a mblock
-  while(temp!=NULL){
-    if(temp->payload == ptr){
-      break;
-    }
+  mblock_t* freeBlock = ptr - MBLOCK_HEADER_SZ;
+  int freedSize = freeBlock->size;
+  if(freeBlock->prev != NULL && freeBlock->prev->status == 0){
+    coallesceBlockPrev(freeBlock);
+    return;
   }
-
+  else if(freeBlock->next != NULL && freeBlock->next->status == 0){
+    coallesceBlockNext(freeBlock);
+  }
 }
 
 void printMemList(const mblock_t* head) {
@@ -78,7 +86,7 @@ mblock_t* findFreeBlockOfSize(size_t size){
   mblock_t* temp = mlist.head;
   mblock_t* lastNode = findLastMemListBlock();
   while(temp!=lastNode){
-    if(temp->status == 0 && temp->size<size){
+    if(temp->status == 0 && temp->size < size){
       return temp;
     }
     temp = temp->next;
@@ -87,44 +95,43 @@ mblock_t* findFreeBlockOfSize(size_t size){
 }
 //Splits one block into two, First one being of size newSize, Second one being the remainder. Update doubly linked list
 void splitBlockAtSize(mblock_t* block, size_t newSize){
-  int freeBlockMemSize = MBLOCK_HEADER_SZ + block->size;
-  mblock_t* remainingBlock;
+  int remBlockSize = MBLOCK_HEADER_SZ + block->size;
+  mblock_t* remainingBlock = block + MBLOCK_HEADER_SZ + block->size;
   remainingBlock->status = 0;
-  remainingBlock->size = freeBlockMemSize - newSize;
-  remainingBlock->payload = NULL;
+  remainingBlock->size = 0;
   remainingBlock->next = block->next;
   remainingBlock->prev = block;
   block->next = remainingBlock;
+  block->size = newSize;
   block->status = 1;
 }
 //Take a block of memory and merge it with the previous block
 void coallesceBlockPrev(mblock_t* freedBlock){
-  mblock_t* freeBlockNext = freedBlock->next;
+  mblock_t* freeBlockPrev = freedBlock->prev;
   freedBlock->prev->size += freedBlock->size;
-  freedBlock = freedBlock->prev;
-  freedBlock->next = freeBlockNext;
+  freedBlock->next->prev = freeBlockPrev;
+  freeBlockPrev->next = freedBlock->next;
 }
 //Take a block of memory and merge it with the next block
 void coallesceBlockNext(mblock_t* freedBlock){
-  mblock_t* freeBlockPrev = freedBlock->prev;
-  freedBlock->next->size += freedBlock->size;
-  freedBlock = freedBlock->next;
-  freedBlock = freeBlockPrev;
+  mblock_t* freeBlockNext = freedBlock->next;
+  freeBlockNext->size += freedBlock->size;
+  freedBlock->prev->next = freeBlockNext;
+  freeBlockNext->prev = freedBlock->prev;
 }
 //Increase heap allocation and create a new memory block in the VA Space
 mblock_t* growHeapBySize(size_t size){
   mblock_t* lastNode = findLastMemListBlock();
   void* p = sbrk(size + MBLOCK_HEADER_SZ);
-  
   if(p==(void*)-1){
     printf("%s", strerror(errno));
     return NULL;
   }
   mblock_t* temp = (mblock_t*)p;
   if(lastNode){
-    temp->prev = lastNode;
     lastNode->next = temp;
   }
+  temp->prev = lastNode;
   temp->next = NULL;
   temp->status = 0;
   temp->size = 0;
